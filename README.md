3befe76f58bb6bf4fd611278245ff72e2878db14
bless4 <36552429+bless4@users.noreply.github.com>
2018-11-03 16:21:22 +0100

    first commit
    freecodecamp

---
+++ README.md
@@ -0,0 +1,1 @@
+#freecodecampjswk2
---
+++ js2wk2.js
@@ -0,0 +1,11 @@
+/*
+The attach is freecodecamp
+javascript2 week2   
+*/
+if (num > 10) {
+    return "No";
+  }
+  if (num < 5) {
+    return "No";
+  }
+  return "Yes";
